70c3aec2b463992ca4bea7362e4a8c2b60d5d4ba
Jovan Slavujevic <jotzilla96@gmail.com>
2023-04-30 00:25:48 +0200

    Added points & grades calculation

--- EIndexClient/src/main/java/eindex/client/MenuScreen.java
+++ EIndexClient/src/main/java/eindex/client/MenuScreen.java
@@ -101,6 +101,22 @@ public class MenuScreen extends javax.swing.JFrame {
             
             float z2 = Float.parseFloat(jZ2.getText());
             jZ2.setForeground((z2 >= 12.5) ? Color.green : Color.red);
+            
+            float points = t1 + t2 + z1 + z2;
+            int grade = (t1 < 12.5 || t2 < 12.5 || z1 < 12.5 || z2 < 12.5 || points < 51) ?
+                    5 : (points < 61) ? 6 : (points < 71) ?
+                    7 : (points < 81) ? 8 : (points < 91) ? 9 : 10;
+            
+            if (grade == 5) {
+                jSummary.setForeground(Color.red);
+                jGrade.setForeground(Color.red);
+            } else {
+                jSummary.setForeground(Color.green);
+                jGrade.setForeground(Color.green);
+            }
+            
+            jSummary.setText(Float.toString(points));
+            jGrade.setText(Integer.toString(grade));
         }
         catch (NumberFormatException e) {
             // should not happen
@@ -151,6 +167,24 @@ public class MenuScreen extends javax.swing.JFrame {
         jZ2 = new javax.swing.JLabel();
         Font fZ2 = jZ2.getFont();
         jZ2.setFont(new Font(fZ2.getName(), fZ2.getStyle() | Font.BOLD, 18));
+        jSummary = new javax.swing.JTextField();
+        jSummary.setEditable(false);
+        Font fSummary = jSummary.getFont();
+        jSummary.setFont(new Font(
+            jSummary.getName(),
+            fSummary.getStyle() | Font.BOLD,
+            18
+        ));
+        jGrade = new javax.swing.JTextField();
+        jGrade.setEditable(false);
+        Font fGrade = jGrade.getFont();
+        jGrade.setFont(new Font(
+            jGrade.getName(),
+            fGrade.getStyle() | Font.BOLD,
+            18
+        ));
+        Grade = new javax.swing.JLabel();
+        Summary = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -170,30 +204,48 @@ public class MenuScreen extends javax.swing.JFrame {
 
         Z2.setText("Z2:");
 
+        Grade.setText("Ocena:");
+
+        Summary.setText("Ukupno:");
+
         javax.swing.GroupLayout jGradesPanelLayout = new javax.swing.GroupLayout(jGradesPanel);
         jGradesPanel.setLayout(jGradesPanelLayout);
         jGradesPanelLayout.setHorizontalGroup(
             jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jGradesPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jGradesPanelLayout.createSequentialGroup()
-                        .addComponent(T1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jT1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jGradesPanelLayout.createSequentialGroup()
+                                .addComponent(T2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jT2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(jGradesPanelLayout.createSequentialGroup()
+                                .addComponent(T1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jT1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(18, 18, 18)
-                        .addComponent(Z1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jZ1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jGradesPanelLayout.createSequentialGroup()
+                                .addComponent(Z2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jZ2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(jGradesPanelLayout.createSequentialGroup()
+                                .addComponent(Z1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jZ1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jGradesPanelLayout.createSequentialGroup()
-                        .addComponent(T2)
+                        .addGap(0, 218, Short.MAX_VALUE)
+                        .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(Summary, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(Grade, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jT2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Z2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jZ2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(264, Short.MAX_VALUE))
+                        .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSummary, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jGrade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(38, 38, 38))))
         );
         jGradesPanelLayout.setVerticalGroup(
             jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -217,7 +269,15 @@ public class MenuScreen extends javax.swing.JFrame {
                             .addComponent(jZ1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jZ2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(174, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 113, Short.MAX_VALUE)
+                .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jSummary, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Summary))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jGradesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jGrade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Grade))
+                .addGap(11, 11, 11))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -275,16 +335,20 @@ public class MenuScreen extends javax.swing.JFrame {
 
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Grade;
     private javax.swing.JLabel SelectSubject;
+    private javax.swing.JLabel Summary;
     private javax.swing.JLabel T1;
     private javax.swing.JLabel T2;
     private javax.swing.JLabel Z1;
     private javax.swing.JLabel Z2;
     private javax.swing.JLabel jFullname;
+    private javax.swing.JTextField jGrade;
     private javax.swing.JPanel jGradesPanel;
     private javax.swing.JLabel jIndex;
     private javax.swing.JLabel jJmbg;
     private javax.swing.JComboBox<String> jSelectSubject;
+    private javax.swing.JTextField jSummary;
     private javax.swing.JLabel jT1;
     private javax.swing.JLabel jT2;
     private javax.swing.JLabel jUsername;
